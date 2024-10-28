9c1c996f3fee53a1c47c7a30f4301de95b7ef5d4
artem Garkushka <Artemgarkushka@gmail.com>
2024-10-28 14:22:03 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+function makeTransaction(quantity, pricePerDroid){
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+
+function makeTransaction(quantity, pricePerDroid){
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+console.log(makeTransaction(3, 1000));
+
+function makeTransaction(quantity, pricePerDroid){
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+console.log(makeTransaction(10, 500));
